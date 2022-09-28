3570a830f24859babc44e55f9ceb12b42f8d85af
coddy83 <coddy083@gmail.com>
2022-09-28 13:13:30 +0900

    fix 간단한 상세페이지

--- script.js
+++ script.js
@@ -4,5 +4,24 @@ const item_info = () => {
 }
 const modal_close = () => {
     const item_detail = document.getElementById("item_detail");
-    item_detail.style.display = "none";
+    item_detail.classList.add("move_left");
+    setTimeout(() => {
+        item_detail.style.display = "none";
+        item_detail.classList.remove("move_left");
+    }
+    , 500);
+}
+
+const market_detail_show = () => {
+    const market_detail = document.getElementById("market_detail");
+    market_detail.style.display = "flex";
+}
+
+const msg_alert = (msg) => {
+    Swal.fire({
+        title: msg,
+        icon: 'success',
+        showConfirmButton: false,
+        timer: 1500
+    })
 }
