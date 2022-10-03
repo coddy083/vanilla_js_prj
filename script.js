e13cadbb9af912ec0fd18264b3d8d5687ee47e83
coddy83 <coddy083@gmail.com>
2022-10-03 17:01:17 +0900

    fix html 나누기?

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 const item_info = () => {
-    const item_detail = document.getElementById("item_detail");
-    item_detail.style.display = "flex";
+    window.location.href = "detail.html";
 }
 const modal_close = () => {
     const item_detail = document.getElementById("item_detail");
     item_detail.classList.add("move_left");
     setTimeout(() => {
-        item_detail.style.display = "none";
+        window.location.href = "index.html";
         item_detail.classList.remove("move_left");
     }
         , 500);
@@ -36,19 +35,19 @@ const market_detail_close = () => {
         , 500);
 }
 
-const market_detail_content_img = () => {
-    const market_detail_content = document.getElementById("market_detail_content");
-    // create img
-    const img_list = []
-    for (let i = 1; i < 5; i++) {
-        const img = document.createElement("img");
-        img.src = `./detail_img/00${i}.jpg`;
-        img_list.push(img);
-        img.classList.add("market_detail_content_img");
-    }
-    // append img
-    for (let i = 0; i < img_list.length; i++) {
-        market_detail_content.appendChild(img_list[i]);
-    }
-}
-market_detail_content_img();
+// const market_detail_content_img = () => {
+//     const market_detail_content = document.getElementById("market_detail_content");
+//     // create img
+//     const img_list = []
+//     for (let i = 1; i < 5; i++) {
+//         const img = document.createElement("img");
+//         img.src = `./detail_img/00${i}.jpg`;
+//         img_list.push(img);
+//         img.classList.add("market_detail_content_img");
+//     }
+//     // append img
+//     for (let i = 0; i < img_list.length; i++) {
+//         market_detail_content.appendChild(img_list[i]);
+//     }
+// }
+// market_detail_content_img();
